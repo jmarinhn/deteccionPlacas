7305f463bccbf0e29d6b47a5b484cc7a692b5385
jdmarinv <jdmarinv@gmail.com>
2022-04-08 16:36:40 -0600

    Mejoras al GUI, cambio a ruta Tesseract

--- gui.py
+++ gui.py
@@ -1,53 +1,57 @@
-from tkinter import *
-from tkinter import filedialog
-from PIL import Image
-from PIL import ImageTk
-import cv2
-import imutils
-
+from tkinter import * 
+import tkinter.font as tkFont
+import os
 def salir():
     window.destroy()
     
-
 def iniciar():
-    global cap
-    cap = cv2.VideoCapture(0,cv2.CAP_DSHOW)
-    visualizar()
+    os.system("python3 deteccionPlacas.py --east frozen_east_text_detection.pb")
+    
 
-def limpiar():
-    labelvideo.image = ""
-    lblvacio.configure(text="")
-    cap.release()
-
-def visualizar():
-    global cap
-    ret, frame = cap.read()
-    if ret == True:
-        frame = imutils.resize(frame,width=640)
-        frame = cv2.cvtColor(frame,cv2.COLOR_BGR2RGB)
-        im = Image.fromarray(frame)
-        img = ImageTk.PhotoImage(im)
-        labelvideo.configure(image=img)
-        labelvideo.image = img
-        labelvideo.after(10,visualizar)
-    else:
-        labelvideo.image = ""
-        lblvacio.configure(text="")
-        cap.release()
-
-cap = None
 window = Tk()
-window.title("REAL-TIME OCR")
-titulo = Label(window, text="REAL-TIME OCR",font=("Arial",24))
-titulo.grid(column=0,row=0,columnspan=1)
-btn = Button(window,text="START",font=("Arial",12),background="gray",fg="white", command=iniciar)
-btn2 = Button(window,text="STOP",font=("Arial",12),background="orange",fg="white", command=limpiar)
-btn3 = Button(window,text="EXIT",font=("Arial",12),background="red",fg="white",command=salir)
-labelvideo = Label(window)
-labelvideo.grid(column=0,row=1,columnspan=2,pady=10)
-btn.grid(column=0,row=2)
-btn2.grid(column=1,row=2)
-btn3.grid(column=2,row=2)
-lblvacio = Label(window,text="",width=20)
-lblvacio.grid(column=4,row=0)
+window.title("Detector de Placas en Tiempo Real")
+
+#setting window size
+width=600
+height=500
+screenwidth = window.winfo_screenwidth()
+screenheight = window.winfo_screenheight()
+alignstr = '%dx%d+%d+%d' % (width, height, (screenwidth - width) / 2, (screenheight - height) / 2)
+window.geometry(alignstr)
+window.resizable(width=False, height=False)
+
+titulo=Label(window)
+ft = tkFont.Font(family='Arial',size=20)
+titulo["font"] = ft
+titulo["fg"] = "#ffffff"
+titulo["justify"] = "center"
+titulo["text"] = "Detector de Placas"
+titulo.place(x=160,y=50,width=292,height=32)
+
+btn = Button(window,text="Iniciar deteccion",font=("Arial",12),background="gray",fg="white", command=iniciar,)
+btn2 = Button(window,text="Salir",font=("Arial",12),background="red",fg="white",command=salir)
+
+btn.place(x=240,y=150,width=144,height=30)
+btn2.place(x=265,y=260,width=70,height=25)
+
+integrantes = ('Grupo #1', 'Josue Marin', 'Jeffrey Herrera', 'Sergio Benitez')
+integrantes_var = StringVar(value=integrantes)
+
+listbox = Listbox(
+    window,
+    listvariable=integrantes_var,
+    height=6,
+    justify="center",
+    selectmode='extended')
+
+listbox.grid(
+    column=0,
+    row=0,
+    sticky='nwes'
+)
+
+listbox.place(x=0,y=420,width=600,height=80)
+
+
 window.mainloop()
+
